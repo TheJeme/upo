1fe7c57593dc99c098bf2018a2409c74490d47ae
Jeme <eemelijoonatan@gmail.com>
2020-12-20 21:48:07 +0200

    updated level03 triangles

--- upo/levels/level03.lua
+++ upo/levels/level03.lua
@@ -11,6 +11,17 @@ function level03:update(dt, timer)
     createCircle(420+timer*7, 1)
     createCircle(420+timer*7, 1)
     createCircle(420+timer*7, 1)
+    
+    trnAngle = math.random(0, 2*math.pi*100) / 100
+
+    createTriangle(gw/2, gh/2, trnAngle+0, 420)
+    createTriangle(gw/2, gh/2, trnAngle+math.pi/2, 420)
+    createTriangle(gw/2, gh/2, trnAngle+math.pi, 420)
+    createTriangle(gw/2, gh/2, trnAngle+3*math.pi/4, 420)
+    createTriangle(gw/2, gh/2, trnAngle+math.pi/4, 420)
+    createTriangle(gw/2, gh/2, trnAngle+7*math.pi/4, 420)
+    createTriangle(gw/2, gh/2, trnAngle+5*math.pi/4, 420)
+    createTriangle(gw/2, gh/2, trnAngle+3*math.pi/2, 420)
   else
     respawnTime = respawnTime + dt
   end
